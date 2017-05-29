6599f26a941ec2e4cc63b4a6b9283c8282f4d600
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-05-28 17:50:33 -0700

    add responsify function

--- client/graphs/word-cloud.js
+++ client/graphs/word-cloud.js
@@ -69,6 +69,7 @@
       .attr('id', 'wordCloud')
       .attr("width", customData.width)
       .attr("height", customData.height)
+			.call(responsivefy)
       .append("g")
       .attr('id', 'g-container')
       .attr("transform", "translate(" + customData.width / 2 + "," + customData.height / 2 + ")")
@@ -118,7 +119,34 @@
   }
 
 
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  function responsivefy(svg) {
+    // get container + svg aspect ratio
+    var container = d3.select(svg.node().parentNode),
+      width = parseInt(svg.style("width")),
+      height = parseInt(svg.style("height")),
+      aspect = width / height;
 
+    // add viewBox and preserveAspectRatio properties,
+    // and call resize so that svg resizes on inital page load
+    svg.attr("viewBox", "0 0 " + width + " " + height)
+      .attr("preserveAspectRatio", "xMinYMid")
+      .call(resize);
+
+    // to register multiple listeners for same event type,
+    // you need to add namespace, i.e., 'click.foo'
+    // necessary if you call invoke this function for multiple svgs
+    // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // get width of container and resize svg to fit it
+    function resize() {
+      var targetWidth = parseInt(container.style("width"));
+      svg.attr("width", targetWidth);
+      svg.attr("height", Math.round(targetWidth / aspect));
+    }
+  }
+	
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
